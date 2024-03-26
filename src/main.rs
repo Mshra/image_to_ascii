6ac327869b985f56322cb7ae65970ff54d4f7bc6
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-03-26 22:39:51 +0530

    edited main

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
 use image::{open, DynamicImage};
 use std::env;
 
-fn open_image(image_name: &str) -> DynamicImage {
-    let image_path: String = format!("images/{}", image_name);
-    let image: DynamicImage = open(image_path).unwrap();
-    image
-}
-
 fn main() {
-    // getting image name through command line arguments
+    // getting image name through as a command line argument
     let args: Vec<String> = env::args().collect();
-    let image = open_image(&args[1])
-        .grayscale()
-        .save("images/amongus_grayscale.png")
-        .unwrap();
 
-    // indexing in a string data type
-    // let hello: String = String::from("Hello, world");
-    // println!("5th element in the list is {:?}", hello.get(5..6));
+    let image_path: String = format!("images/{}", &args[1]);
+
+    let image: DynamicImage = open(&image_path).unwrap();
+    image.grayscale();
+
+    let width: u32 = image.width();
+    let height: u32 = image.height();
+    let gray_ramp: String =
+        String::from("$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,\"^`'. ");
 }
